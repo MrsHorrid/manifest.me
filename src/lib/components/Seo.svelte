dec08282805a770dc6dc2eb73efb4aa50ac33211
Shaked Lanciano <shakedlanciano@gmail.com>
2023-03-17 23:19:04 +0200

    as

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -1,6 +1,6 @@
 <script></script>
 
-<svele:head>
+<svelte:head>
     <title>Manifest.me</title>
     <meta name="description" content="a simple app to help you manifest your desires">
     <meta property="og:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086389074937532536/ADasdas.jpg">
@@ -23,4 +23,4 @@
     <meta name="twitter:image:secure_url" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086389074937532536/ADasdas.jpg">
     <meta name="twitter:image:alt" content="A simple app to help you manifest your desires">
     <meta name="twitter:description" content="a simple app to help you manifest your desires">
-</svele:head>
+</svelte:head>
