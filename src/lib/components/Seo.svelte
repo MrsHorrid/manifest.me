13615e01539093ce8d3d43eb2db354ece270328b
Shaked Lanciano <shakedlanciano@gmail.com>
2023-03-17 23:40:36 +0200

    ahahaha

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -5,27 +5,27 @@
     <meta name="description" content="a simple app to help you manifest your desires">
 
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086389074937532536/ADasdas.jpg">
+    <meta name="twitter:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
     <meta name="twitter:image:alt" content="A simple app to help you manifest your desires">
     <meta name="twitter:title" content="Manifest.me">
     <meta name="twitter:description" content="a simple app to help you manifest your desires">
     <meta name="twitter:url" content="https://manifest-me.vercel.app/">
     <meta name="twitter:site" content="@manifest_me">
     <meta name="twitter:creator" content="@HorridWeasell">
-    <meta name="twitter:image:src" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086389074937532536/ADasdas.jpg">
-    <meta name="twitter:image:width" content="800">
-    <meta name="twitter:image:height" content="600">
-    <meta name="twitter:image:type" content="image/jpg">
-    <meta name="twitter:image:secure_url" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086389074937532536/ADasdas.jpg">
+    <meta name="twitter:image:src" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
+    <meta name="twitter:image:width" content="1920">
+    <meta name="twitter:image:height" content="1080">
+    <meta name="twitter:image:type" content="image/png">
+    <meta name="twitter:image:secure_url" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
     <meta name="twitter:image:alt" content="A simple app to help you manifest your desires">
     <meta name="twitter:description" content="a simple app to help you manifest your desires">
     <!-- meta article -->
-    <meta property="og:type" content="article">
+    <meta property="og:type" content="website">
     <meta property="og:url" content="https://manifest-me.vercel.app/">
     <meta property="og:title" content="Manifest.me">
     <meta property="og:description" content="a simple app to help you manifest your desires">
-    <meta property="og:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086389074937532536/ADasdas.jpg">
-    <meta property="og:image:width" content="800">
-    <meta property="og:image:height" content="600">
-    <meta property="og:image:type" content="image/jpg">
+    <meta property="og:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
+    <meta property="og:image:width" content="1920">
+    <meta property="og:image:height" content="1080">
+    <meta property="og:image:type" content="image/png">
 </svelte:head>
