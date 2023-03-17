9693527537302d4a029bbef46c84f0d2330fd9a4
Shaked Lanciano <shakedlanciano@gmail.com>
2023-03-17 22:54:11 +0200

    oioioi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import AddManifest from "$lib/components/AddManifest.svelte";
-    import { MetaTags } from 'svelte-meta-tags'
+   
 </script>
 
 <div class="p-8 md:p-20">
@@ -8,32 +8,3 @@
     <AddManifest />
 </div>
 
-<MetaTags
-  title="Manifest.me"
-  titleTemplate="%s | Svelte Meta Tags"
-  description="A simple app to help you manifest your desires"
-  openGraph={{
-    type: 'website',
-    url: 'https://manifest-me.vercel.app/',
-    title: 'Manifest.me',
-    description: 'A simple app to help you manifest your desires',
-    images: [
-      {
-        url: 'https://imgur.com/VmK3XI6',
-        width: 800,
-        height: 600,
-        alt: 'manifest.me preview image'
-      },
-    ],
-  }}
-  twitter={{
-    handle: '@HorridWeasell',
-    site: '@https://manifest-me.vercel.app/',
-    cardType: 'summary_large_image',
-    title: 'Manifest.me',
-    description: 'A simple app to help you manifest your desires',
-    image: 'https://imgur.com/VmK3XI6',
-    imageAlt: 'Twitter image alt'
-  }}
-
-/>
