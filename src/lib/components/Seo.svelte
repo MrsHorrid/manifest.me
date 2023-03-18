ebc00aa54af3b1e495858c56963f5fe7b98da7d0
Shaked Lanciano <shakedlanciano@gmail.com>
2023-03-18 15:04:01 +0200

    fixed mobile hopefully

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -3,25 +3,6 @@
 <svelte:head>
     <title>Manifest.me</title>
     <meta name="description" content="a simple app to help you manifest your desires">
-
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
-    <meta name="twitter:image:alt" content="A simple app to help you manifest your desires">
-    <meta name="twitter:title" content="Manifest.me">
-    <meta name="twitter:description" content="a simple app to help you manifest your desires">
-    <meta name="twitter:url" content="https://manifest-me.vercel.app/">
-    <meta name="twitter:site" content="@manifest_me">
-    <meta name="twitter:creator" content="@HorridWeasell">
-    <meta name="twitter:image:src" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
-    <meta name="twitter:image:width" content="1920">
-    <meta name="twitter:image:height" content="1080">
-    <meta name="twitter:image:type" content="image/png">
-    <meta name="twitter:image:secure_url" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
-    <meta name="twitter:image:alt" content="A simple app to help you manifest your desires">
-    <meta name="twitter:description" content="a simple app to help you manifest your desires">
-    <!-- meta article -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://manifest-me.vercel.app/">
     <meta property="og:title" content="Manifest.me">
     <meta property="og:description" content="a simple app to help you manifest your desires">
     <meta property="og:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
