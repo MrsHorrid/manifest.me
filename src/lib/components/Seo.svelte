1a0221e8a7f22504e845eded86ba159e0f7795c8
Shaked Lanciano <shakedlanciano@gmail.com>
2023-03-24 10:24:23 +0300

    fixed mobile

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -6,7 +6,5 @@
     <meta property="og:title" content="Manifest.me">
     <meta property="og:description" content="a simple app to help you manifest your desires">
     <meta property="og:image" content="https://cdn.discordapp.com/attachments/1063591220028383252/1086376403810926613/Slide_16_9_-_1.png">
-    <meta property="og:image:width" content="1920">
-    <meta property="og:image:height" content="1080">
     <meta property="og:image:type" content="image/png">
 </svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="p-8 md:p-20">
-    <h1 class="font-bold text-[20px]">What would you like to <span class="text-[#8D86C9]">manifest</span> into your life right <span class="text-[#8D86C9]">now</span>?</h1>
+    <h1 class="font-bold text-[20px] xs:text-3xl">What would you like to <span class="text-[#8D86C9]">manifest</span> into your life right <span class="text-[#8D86C9]">now</span>?</h1>
     <AddManifest />
 </div>
 
